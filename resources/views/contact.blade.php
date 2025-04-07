611f424f11e7f2a70b5bd680e6dfed38cdd3b800
UlysseLeZinzin <lebouteiller.ulysse@gmail.com>
2025-04-07 15:08:28 +0200

    d

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ config('app.name') }} | Contact</title>
 </head>
 <body>
     <h1>Contact</h1>
